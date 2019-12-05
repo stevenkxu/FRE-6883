a184efb1d85a8978aee669c2230d7f27909d023b
Steven Xu <stevenxu@att.net>
2019-12-05 16:21:22 -0500

    Creating class to hold functionality Finalizing shuffle functionality introducing getting functionality

--- SelectRandomStocks.cpp
+++ SelectRandomStocks.cpp
@@ -3,28 +3,46 @@
 #include <vector>
 #include <string>
 #include "EquityDivide.h"
+#include "SelectRandomStocks.h"
 
+
+void StockShuffler::ShuffleStocks() {
+	shuffle(stock_groups[0].begin(), stock_groups[0].end(), default_random_engine);
+	shuffle(stock_groups[1].begin(), stock_groups[1].end(), default_random_engine);
+	shuffle(stock_groups[2].begin(), stock_groups[2].end(), default_random_engine);
+}
+
+void Get30StocksPerGroup() {
+
+}
+// will probably put all of this into a class wrapper
 // right now, this is a placeholder: these numbers will be the size of each of the groups in Group
-int size_miss;
-int size_meet;
-int size_beat;
+//int size_miss;
+//int size_meet;
+//int size_beat;
+//
+//
+//// a vector of random integers that will serve as indices (proof of concept)
+//vector<int> numbers_miss;
+//vector<int> numbers_meet;
+//vector<int> numbers_beat;
+//
+//for (int i = 0; i < size_miss; i++) {
+//	numbers_miss.push_back(i);
+//}
+//for (int i = 0; i < size_meet; i++) {
+//	numbers_meet.push_back(i);
+//}
+//for (int i = 0; i < size_beat; i++) {
+//	numbers_meet.push_back(i);
+//}
+//
+//shuffle(numbers_miss.begin(), numbers_miss.end(), default_random_engine);
+//shuffle(numbers_meet.begin(), numbers_meet.end(), default_random_engine);
+//shuffle(numbers_beat.begin(), numbers_beat.end(), default_random_engine);
 
+//alternate idea: generate a group and just shuffle the vectors.
+//EquityDivide equitydivider= EquityDivide("/EPS/EPS.csv");
+//Group stock_groups;
+//stock_groups = equitydivider.divide_group();
 
-// a vector of random integers that will serve as indices
-vector<int> numbers_miss;
-vector<int> numbers_meet;
-vector<int> numbers_beat;
-
-for (int i = 0; i < size_miss; i++) {
-	numbers_miss.push_back(i);
-}
-for (int i = 0; i < size_meet; i++) {
-	numbers_meet.push_back(i);
-}
-for (int i = 0; i < size_beat; i++) {
-	numbers_meet.push_back(i);
-}
-
-shuffle(numbers_miss.begin(), numbers_miss.end(), default_random_engine);
-shuffle(numbers_meet.begin(), numbers_meet.end(), default_random_engine);
-shuffle(numbers_beat.begin(), numbers_beat.end(), default_random_engine)
