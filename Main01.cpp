1bad84c929b2d801400c225e019e1965ec956eb5
Steven Xu <stevenxu@att.net>
2019-12-06 13:54:41 -0500

    Commenting Main01 to explain how my implementation of loop logic works

--- Main01.cpp
+++ Main01.cpp
@@ -9,10 +9,18 @@ int main() {
 	StockShuffler shuffler = StockShuffler(divided_stocks);
 	
 	for (int i = 0; i < 3; i++) {
+		//This code will randomly output 30 stocks!
 		Group empty_stocks(3);
 		shuffler.ShuffleStocks();
 		shuffler.Get30StocksPerGroup(empty_stocks);
-		cout << empty_stocks[0][1].first << endl;
+
+		// This is how you can get the ticker of the 30 stocks! Right now, the functionality is to cout them, but this can be used as a string input for the next part of the program
+		for (int j = 0; j < 30; j++) {
+			cout << empty_stocks[0][j].first << " ";
+		}
+
+
+		cout << endl;
 		cout << empty_stocks[0].size() << endl;
 	}
 	
